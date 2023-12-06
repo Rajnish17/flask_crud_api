5f6b35b8a6913573c3c3f751eb1755b6c2666f7d
Rajnish <RajnishAryan12@gmail.com>
2023-12-06 09:44:12 +0530

    post route added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -12,7 +12,15 @@ def hello():
 @app.route("/api/data", methods=["GET"])
 def get_data():
     data = {"key": "value", "number": 42}
-    return jsonify(data)
+    return jsonify(data),200
+
+
+@app.route("/post", methods=["POST"])
+def post_data():
+    if request.is_json:
+        data = request.get_json()
+        print("received JSON data: ", data)
+        return jsonify(data),201
 
 
 if __name__ == "__main__":
