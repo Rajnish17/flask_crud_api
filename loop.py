622e5245086dd588a1071758237e47b4e5a4c2b8
Rajnish <RajnishAryan12@gmail.com>
2023-12-06 19:03:08 +0530

    loop in python

---
+++ loop.py
@@ -0,0 +1,12 @@
+
+# for i in range(10):
+    # print(i)
+
+nums=10
+if(nums < 10):
+    print(nums)
+else:
+    print("number is greater than 10")
+my_name="Rajnish"    
+print(type(my_name))
+print(my_name)
