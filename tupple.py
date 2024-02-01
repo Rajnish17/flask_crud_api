219dd0bb0df8a2b489d80fe01b403e0cf3aec69a
Rajnish <RajnishAryan12@gmail.com>
2024-02-01 18:07:27 +0530

    tupple added

--- tupple.py
+++ tupple.py
@@ -4,8 +4,15 @@ arr=("rajnish",1,"aman",5165)
 
 print(arr[3])
 print(arr[0])
+ 
 
+#  add in tuple
+new_arr =arr+("fdjbjhbf","djhsjbhd",6554654561,0.23)
 
+print(arr)
+
+print(new_arr)
 
 for i in arr:
     print(i)
+print(len(arr))
