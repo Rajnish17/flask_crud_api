c866c83c3e4038280c6c9bb6a018f37fb28b821a
Rajnish <RajnishAryan12@gmail.com>
2024-02-10 15:00:24 +0530

    dictonary added

---
+++ dictonary.py
@@ -0,0 +1,26 @@
+student ={
+  1:{
+   "name":"rajnish",
+   "email":"rajnish@gmail.com",
+   "marks":"100"
+  },
+  2:{
+   "name":"rajnish aryan",
+   "email":"rajnish@gmail.com",
+   "marks":"100"
+  },
+  3:{
+   "name":"aman",
+   "email":"rajnish@gmail.com",
+   "marks":"100"
+  },
+  4:{
+   "name":"rajan",
+   "email":"rajnish@gmail.com",
+   "marks":"100"
+  },
+
+ }
+
+print(student[1]["name"])
+print("marks",4)
